8cd07542f6a743f8a8b2ee7037a58aca9fe0924e
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2023-07-05 19:24:27 +0200

    task 9 fixed

--- js/task-09.js
+++ js/task-09.js
@@ -3,15 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-let colorCatcher = getRandomHexColor();
 
 const colorButton = document.querySelector(".change-color");
-colorButton.addEventListener("click", function onClick() {
-  document.body.style.backgroundColor = colorCatcher;
-});
 
-const backgroundColor = document.querySelector(".color");
-backgroundColor.innerHTML = colorCatcher;
+colorButton.addEventListener("click", function onClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  const backgroundColor = document.querySelector(".color");
+  let colorCatcher = getRandomHexColor();
+  backgroundColor.innerHTML = colorCatcher;
+});
 
 // proszę jeszcze wypisać kolor do html'a.
 // można zapisywać otrzymany kolor po pierwszym wywołaniu
